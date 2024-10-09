1662549e18c8858aaf712b6b2d168c08f1917758
hashe_lee <150456150+hashelee@users.noreply.github.com>
2024-10-09 22:27:33 +0530

    commit order table changes

--- models/order_model.js
+++ models/order_model.js
@@ -14,7 +14,13 @@ function createOrderTable() {
     orderCompletedDateTime DATETIME,
     special_notes VARCHAR(255),
     hotel_id INT,
-    FOREIGN KEY (hotel_id) REFERENCES hotel(id) ON DELETE CASCADE
+    FOREIGN KEY (hotel_id) REFERENCES hotel(id) ON DELETE CASCADE,
+    laundry_id INT,
+    FOREIGN KEY (laundry_id) REFERENCES laundry(id) ON DELETE CASCADE,
+    pickup_delivery_rider_id INT,
+    FOREIGN KEY (pickup_delivery_rider_id) REFERENCES deliveryRiders(id) ON DELETE CASCADE,
+    drop_delivery_rider_id INT,
+    FOREIGN KEY (drop_delivery_rider_id) REFERENCES deliveryRiders(id) ON DELETE CASCADE
   );
   `;
 
