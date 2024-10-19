255ab635b6e435cc4e3aac79eaad531757bef09b
hashe_lee <150456150+hashelee@users.noreply.github.com>
2024-10-20 00:48:12 +0530

    add authentication of create order

--- routes/management/hotelRoute.js
+++ routes/management/hotelRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {
     getHotelDetailsById,
     getOrdersByHotelId,
+    requestOrderToLaundry,
     declineOrderByHotel
 } = require('../../controllers/management/hotelController');
 
@@ -12,6 +13,8 @@ const authenticateToken = require('../../middleware/auth');
 router.get('/:hotel_id/details', authenticateToken, getHotelDetailsById)
 
 router.get('/:hotel_id/orders', authenticateToken, getOrdersByHotelId)
+
+router.post('/:hotel_id/request-laundry',authenticateToken, requestOrderToLaundry)
 
 router.put('/:hotel_id/orders/:order_id/decline', authenticateToken, declineOrderByHotel)
 
